d5f87203784253e81cb15e34d6c157d3077a3476
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-10-17 08:31:31 +0100

    Bayesian Machine

--- src/gp/bcm.py
+++ src/gp/bcm.py
@@ -9,10 +9,16 @@ Description: Functions related to the Gaussian Process
 import torch
 import numpy as np
 from sklearn.cluster import KMeans
+from torch.multiprocessing import Pool, Process, set_start_method
 
 # our script and functions
 from src.gp.kernel import solve, logdeterminant, compute_kernel
 from src.gp.transformation import PreWhiten, Normalisation
+
+try:
+    set_start_method('spawn')
+except RuntimeError:
+    pass
 
 
 def clustering(xsamples: torch.Tensor, ysamples: torch.Tensor, n_clusters: int) -> dict:
@@ -96,6 +102,34 @@ def log_marginal_likelihood_parallel(args: list) -> list:
     jitter = args[2]
     log_ml = log_marginal_likelihood(kernel, targets, jitter)
     return log_ml
+
+
+def cost_parallel(record: dict, hyperparameters: torch.Tensor, jitter: float) -> torch.Tensor:
+    """Calculates the total cost given a dictionary consisting of the
+    partitioned data and the hyperparameters.
+
+    Args:
+        record (dict): A dictionary containing the partitioned data
+        hyperparameters (torch.Tensor): The kernel hyperparameter.
+        jitter (float): The jitter term for numerical stability
+
+    Returns:
+        torch.Tensor: The total cost
+    """
+    nclusters = len(record)
+    arguments = list()
+    for i in range(nclusters):
+        xpoint, ypoint = record[str(i)][0], record[str(i)][1]
+        kernel = compute_kernel(xpoint, xpoint, hyperparameters)
+        arguments.append([kernel, ypoint, jitter])
+
+    nprocesses = torch.multiprocessing.cpu_count()
+    pool = Pool(processes=nprocesses)
+    costs = pool.map(log_marginal_likelihood_parallel, arguments)
+    pool.close()
+    pool.join()
+
+    return torch.FloatTensor(costs).sum(0)
 
 
 def cost(record: dict, hyperparameters: torch.Tensor, jitter: float) -> torch.Tensor:
