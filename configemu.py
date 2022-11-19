0490c56d2aefe8f690054398d99b20335e26d3d3
Harry45 <arrykrish@gmail.com>
2022-11-19 10:21:17 +0000

    configurations

---
+++ configemu.py
@@ -0,0 +1,86 @@
+"""
+Project: Scalable Gaussian Process Emulator (SUPER) for modelling power spectra
+Author: Dr. Arrykrishna Mootoovaloo
+Date: November 2022
+Email: arrykrish@gmail.com
+Description: The main configuration file
+"""
+
+from ml_collections.config_dict import ConfigDict
+
+BF_PARAMS = {'AGN': {'A2': -0.11900, 'B2': 0.1300, 'C2': 0.6000, 'D2': 0.002110, 'E2': -2.0600,
+                     'A1': 0.30800, 'B1': -0.6600, 'C1': -0.7600, 'D1': -0.002950, 'E1': 1.8400,
+                     'A0': 0.15000, 'B0': 1.2200, 'C0': 1.3800, 'D0': 0.001300, 'E0': 3.5700},
+             'REF': {'A2': -0.05880, 'B2': -0.2510, 'C2': -0.9340, 'D2': -0.004540, 'E2': 0.8580,
+                     'A1': 0.07280, 'B1': 0.0381, 'C1': 1.0600, 'D1': 0.006520, 'E1': -1.7900,
+                     'A0': 0.00972, 'B0': 1.1200, 'C0': 0.7500, 'D0': -0.000196, 'E0': 4.5400},
+             'DBLIM': {'A2': -0.29500, 'B2': -0.9890, 'C2': -0.0143, 'D2': 0.001990, 'E2': -0.8250,
+                       'A1': 0.49000, 'B1': 0.6420, 'C1': -0.0594, 'D1': -0.002350, 'E1': -0.0611,
+                       'A0': -0.01660, 'B0': 1.0500, 'C0': 1.3000, 'D0': 0.001200, 'E0': 4.4800}}
+
+
+def get_config() -> ConfigDict:
+    """Generates the main configuration file for Class and the emulator. Note that the wavenumber is in inverse Mpc.
+
+    Returns:
+        ConfigDict: the configuration file
+    """
+
+    config = ConfigDict()
+
+    # parameters
+    config.parameters = parameters = ConfigDict()
+    parameters.keys = ['omega_cdm', 'omega_b', 'ln10^{10}A_s', 'n_s', 'h']
+    parameters.distribution = 'uniform'
+    parameters.loc = [0.06, 0.019, 1.70, 0.70, 0.64]
+    parameters.scale = [0.34, 0.007, 3.30, 0.60, 0.18]
+    parameters.reference = [0.12, 0.020, 3.0, 1.0, 0.70]
+
+    # boolean settings
+    config.boolean = boolean = ConfigDict()
+    boolean.neutrino = False
+    boolean.baryonfeedback = False
+
+    # settings for analytical baryon feedback model
+    config.bf = bar_fed = ConfigDict()
+    bar_fed.model = 'AGN'
+    bar_fed.params = BF_PARAMS[config.bar_fed.model]
+
+    # CLASS settings
+    config.classy = classy = ConfigDict()
+    classy.halofit_k_per_decade = 80
+    classy.halofit_sigma_precision = 0.05
+    classy.output = "mPk"
+    classy.mode = 'hmcode'
+    classy.bbn = '/home/harry/Desktop/class/bbn/sBBN.dat'
+    classy.k_pivot = 0.05
+    classy.Omega_k = 0.0
+    classy.k_max_pk = 50
+    classy.z_max_pk = 5.0
+
+    # neutrino settings
+    config.neutrino = neutrino = ConfigDict()
+    neutrino.N_ncdm = 1.0
+    neutrino.deg_ncdm = 3.0
+    neutrino.T_ncdm = 0.71611
+    neutrino.N_ur = 0.00641
+    neutrino.fixed_m = 0.06
+
+    # emulator settings
+    config.emulator = emulator = ConfigDict()
+    emulator.niter = 500
+    emulator.lr = 1E-2
+    emulator.nrestart = 3
+    emulator.zmin = 0.0
+    emulator.zmax = 5.0
+    emulator.kmin = 5E-4
+    emulator.kmax = 50
+    emulator.gridz = 20
+    emulator.gridk = 40
+
+    # spline interpolator settings
+    config.interp = interp = ConfigDict()
+    interp.gridz = 100
+    interp.gridk = 1000
+
+    return config
---
+++ src/cosmo/argsgen.py
@@ -0,0 +1,70 @@
+"""
+Project: Scalable Gaussian Process Emulator (SUPER) for modelling power spectra
+Author: Dr. Arrykrishna Mootoovaloo
+Date: November 2022
+Email: arrykrish@gmail.com
+Description: Generate the dictionaries to be passed to Class to compute the linear/non-linear matter power spectrum.
+"""
+
+
+from ml_collections.config_dict import ConfigDict
+
+
+def class_args(config: ConfigDict) -> dict:
+    """Generates CLASS arguments to be passed to classy to compute the different quantities.
+
+    Args:
+        config (ConfigDict): A configuration file containing the parameters.
+
+    Returns:
+        dict: A dictionary to input to class
+    """
+    dictionary = dict()
+    dictionary['output'] = config.classy.output
+    dictionary['P_k_max_1/Mpc'] = config.classy.k_max_pk
+    dictionary['z_max_pk'] = config.classy.z_max_pk
+    dictionary['sBBN file'] = config.classy.bbn
+    dictionary['k_pivot'] = config.classy.k_pivot
+    dictionary['Omega_k'] = config.classy.Omega_k
+    return dictionary
+
+
+def neutrino_args(config: ConfigDict, **kwargs) -> dict:
+    """Generates a dictionary for the neutrino settings.
+
+    Args:
+        config (ConfigDict): The main configuration file
+
+    Returns:
+        dict: A dictionary with the neutrino parameters.
+    """
+    dictionary = dict()
+    dictionary['N_ncdm'] = config.neutrino.N_ncdm
+    dictionary['deg_ncdm'] = config.neutrino.deg_ncdm
+    dictionary['T_ncdm'] = config.neutrino.T_ncdm
+    dictionary['N_ur'] = config.neutrino.N_ur
+
+    if 'M_tot' in kwargs and 'M_tot' in config.parameters.keys:
+        mass = kwargs.pop('M_tot')
+    else:
+        mass = config.neutrino.fixed_nm
+    dictionary['m_ncdm'] = mass / config.neutrino.deg_ncdm
+    return dictionary
+
+
+def params_args(config: ConfigDict, values: list) -> dict:
+    """Generates a dictionary of parameters, which will be used as input to Classy
+
+    Args:
+        config (ConfigDict): the main configuration file
+        values (list): the values of the parameters
+
+    Returns:
+        dict: a dictionary, with the approximate keys and values
+    """
+    assert len(values) == len(config.parameters.keys), 'Mis-match in shape'
+
+    dictionary = dict()
+    for i, key in enumerate(config.parameters.keys):
+        dictionary[key] = values[i]
+    return dictionary
